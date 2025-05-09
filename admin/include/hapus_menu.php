2e61ba2a8309459a9faeb2c9bbefec0dde9dd70e
AldyAbdullah <aldiabdullah322@gmail.com>
2025-05-09 23:07:43 +0800

    fix bug hapus menu

--- admin/include/hapus_menu.php
+++ admin/include/hapus_menu.php
@@ -5,10 +5,10 @@ if (isset($_GET['id'])) {
     $id = $_GET['id'];
 
     try {
-        $stmt = $pdo->prepare("DELETE FROM tb_buah WHERE id_buah = :id");
+        $stmt = $pdo->prepare("DELETE FROM tb_menu WHERE id_menu = :id");
         $stmt->execute([':id' => $id]);
 
-        echo "Data berhasil dihapus. <meta http-equiv='refresh' content='1; url=index.php?page=data_buah'/>";
+        echo "Data berhasil dihapus. <meta http-equiv='refresh' content='1; url=index.php?page=data_menu'/>";
     } catch (PDOException $e) {
         echo "Gagal menghapus data: " . $e->getMessage();
     }
